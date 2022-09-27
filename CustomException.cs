85cd792d2a1d06fe6f88c5df575ba612ba752407
Natalie Thiele <natalie.a.a.thiele@gmail.com>
2022-09-27 16:04:23 -0400

    Add algo thinking to SoreLoserPlayer

--- CustomException.cs
+++ CustomException.cs
@@ -1,21 +1,21 @@
-using System;
+// using System;
 
-//I think I have to create a custom exception for the Sore Loser when they lose. I am not sure what constructors/etc. need to be here. this is from the microsoft page https://learn.microsoft.com/en-us/dotnet/standard/exceptions/how-to-create-localized-exception-messages
-//https://learn.microsoft.com/en-us/dotnet/standard/exceptions/how-to-create-user-defined-exceptions?source=recommendations
+// //I think I have to create a custom exception for the Sore Loser when they lose. I am not sure what constructors/etc. need to be here. this is from the microsoft page https://learn.microsoft.com/en-us/dotnet/standard/exceptions/how-to-create-localized-exception-messages
+// //https://learn.microsoft.com/en-us/dotnet/standard/exceptions/how-to-create-user-defined-exceptions?source=recommendations
 
-namespace ShootingDice
-{
+// namespace ShootingDice
+// {
 
-    public class LoserException : Exception
-    {
-        //no idea how to make this ... constructor? work?
-        public LoserException() {}
+//     public class LoserException : Exception
+//     {
+//         //no idea how to make this ... constructor? work?
+//         public LoserException() {}
 
-        //I think these are if we need a message to pop up? I do not know if I should have these.
-        public LoserException(string message)
-            : base(message) {}
-        public LoserException(string message, Exception inner)
-            : base(message, inner) {}
-    }
+//         //I think these are if we need a message to pop up? I do not know if I should have these.
+//         public LoserException(string message)
+//             : base(message) {}
+//         public LoserException(string message, Exception inner)
+//             : base(message, inner) {}
+//     }
 
-}
+// }
--- SoreLoserPlayer.cs
+++ SoreLoserPlayer.cs
@@ -6,7 +6,7 @@ namespace ShootingDice
 
     // A Player that throws an exception when they lose to the other player
     // Where might you catch this exception????
-    //is this a custom exception?
+    //is this a custom exception? -- it can be any exception i want to break the code
     //"Try" is the roll up to the winning message
     //"Catch" is ... custom or other exception? ... then maybe a "Flips the table" or "I can't lose" or something message?
     public class SoreLoserPlayer : Player
@@ -16,6 +16,9 @@ namespace ShootingDice
             // Call roll for "this" object and for the "other" object
             int myRoll = Roll();
             int otherRoll = other.Roll();
+            //if (roll means player loses)
+            //throw new InvalidOperationException();
+            //then do a try/catch
 
             Console.WriteLine($"{Name} rolls a {myRoll}");
             Console.WriteLine($"{other.Name} rolls a {otherRoll}");

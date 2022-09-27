caecbac1ed9350c1098aaa683d737d1f76452df8
Natalie Thiele <natalie.a.a.thiele@gmail.com>
2022-09-27 15:31:22 -0400

    Setup for SoreLoser, some algo thoughts

--- SoreLoserPlayer.cs
+++ SoreLoserPlayer.cs
@@ -1,11 +1,37 @@
+using System;
+
 namespace ShootingDice
 {
     // TODO: Complete this class
 
     // A Player that throws an exception when they lose to the other player
     // Where might you catch this exception????
-    public class SoreLoserPlayer
+    //is this a custom exception?
+    //"Try" is the roll up to the winning message
+    //"Catch" is ... custom or other exception? ... then maybe a "Flips the table" or "I can't lose" or something message?
+    public class SoreLoserPlayer : Player
     {
-        
+        public override void Play(Player other)
+        {
+            // Call roll for "this" object and for the "other" object
+            int myRoll = Roll();
+            int otherRoll = other.Roll();
+
+            Console.WriteLine($"{Name} rolls a {myRoll}");
+            Console.WriteLine($"{other.Name} rolls a {otherRoll}");
+            if (myRoll > otherRoll)
+            {
+                Console.WriteLine($"{Name} Wins!");
+            }
+            else if (myRoll < otherRoll)
+            {
+                Console.WriteLine($"{other.Name} Wins!");
+            }
+            else
+            {
+                // if the rolls are equal it's a tie
+                Console.WriteLine("It's a tie");
+            }
+        }
     }
 }
